9813c145217f9410a7361cb512408c30d1e4b5f2
himanshusharmamitrc <himanshusharmamitrc18@gmail.com>
2026-07-20 22:54:04 +0530

    Day 1: Python fundamentals and VS Code setup

---
+++ day1/1_hello.py
@@ -0,0 +1,3 @@
+print("Hello, Github!")
+print("My name is Himanshu Sharma")
+print("Today is my first day of Python fundamentals ")
---
+++ day1/2_variables.py
@@ -0,0 +1,10 @@
+name = "Himanshu Sharma"
+age = 20
+course = "Btech,AI/ML"
+college = "MITRC"
+dream = "I want to become a AI/ML Engineer"
+print("NAME:", name)
+print("AGE:",age)
+print("COURSE:", course)
+print("COLLEGE:", college)
+print("DREAM:", dream)
