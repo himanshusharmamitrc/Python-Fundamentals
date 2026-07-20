07d56625e06edd5cb9657442cde3ee1dc4395ed0
himanshusharmamitrc <himanshusharmamitrc18@gmail.com>
2026-07-20 22:55:48 +0530

    Day 3: For and while loops

---
+++ day3/1_for_loop.py
@@ -0,0 +1,36 @@
+# using for loop  
+for i in range(5):
+    print("Himanshu Sharma")
+
+print()
+
+#This code will print numbers from 1 to 5
+for i in range(5):
+    print(i + 1)
+
+print()
+
+#This code will print reverse number from 10 to 1
+for i in range(10, 0, -1):
+    print(i)
+
+print()    
+
+# Print even numbers from 1 to 10 using a for loop
+for i in range (2,11,2):
+    print (i)    
+
+print()
+
+# Print odd numbers from 1 to 10 using a for loop
+for i in range (1,10,2):
+    print(i)
+
+print()    
+
+#Print multiplication table of 5 using a for loop
+number = int(input("Enter a number "))
+for i in range(1,11):
+    print(number,"X",i, "=" ,number*i)
+
+print()    
---
+++ day3/2_while_loop.py
@@ -0,0 +1,39 @@
+# Print numbers from 1 to 5 using a while loop
+i = 1
+while i<= 5:
+    print(i)
+    i= i + 1
+
+print()
+
+# Print numbers from 10 to 1 using a while loop
+i = 10
+
+while i >= 1:
+    print(i)
+    i = i - 1
+
+print()
+
+# Print even numbers from 1 to 10 using a while loop
+i = 2
+while i<= 10:
+    print(i)
+    i =i + 2
+
+print()
+
+# Print odd numbers from 1 to 10 using a while loop
+i = 1
+while i<= 10:
+    print(i)
+    i =i + 2    
+
+print()
+
+#print multiplication table of a number using while loop
+number = int(input("Enter a number: "))
+i = 1
+while i <= 10:
+    print(number, "X", i, "=", number * i)
+    i = i + 1
